2caa7f5dfad0370ed0d802ad04f489051c21a6c2
5N6R <5n6r_2017@mail.ru>
2017-01-07 07:52:40 +0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-print("\033[2;32mThis is my first program\n")
-print(chr(169)+" 2017, Coded by 5.N.6.R.\n")
-input("For exit press <ENTER>\033[0;0m")
-
+print("\033[1;32m+---------------------------+")
+print("| This is my first program  |")
+print("| "+chr(169)+" 2017, Coded by 5.N.6.R. |")
+print("+---------------------------+")
+print("|  For exit press <ENTER>   |")
+print("+---------------------------+\033[0;0m")
+input()
